cbe0d56f9fc216d7d77c20c8d6a97d529933b453
Cole Challand <ctchalland@gmail.com>
2018-08-13 13:11:56 -0500

    Fancy Javascript

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -11,7 +11,7 @@ class RequestIndex extends Component {
     const requestCount = await campaign.methods.getRequestsCount().call();
 
     const requests = await Promise.all(
-      Array(parseInt(requestCount)).fill().map((Element, index) => {
+      Array(parseInt(requestCount)).fill().map((element, index) => {
         return campaign.methods.requests(index).call()
       })
     );
